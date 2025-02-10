7687513492f25be2f6687c9ff628161b97bb35b6
VictorMadronaSanz <165796795+VictorMadronaSanz@users.noreply.github.com>
2025-02-10 21:40:57 +0100

    Cambios Irrelevantes

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private TimeManager time;
+    private int score;
+    private int highScore;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //score = 10000 - (time + distance);
+
+}
