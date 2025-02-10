55569776d3971e0abd38fda7da7d92604200abd2
VictorMadronaSanz <165796795+VictorMadronaSanz@users.noreply.github.com>
2025-02-10 22:07:45 +0100

    Intento Score

---
+++ Assets/Scripts/ShowScore.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //GetComponent<Text>().text = "Score: " + ScoreManager.Instance.Score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
